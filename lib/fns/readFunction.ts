9a189145a070a6c733bef36553c50d88daf3f1e1
ahisley <ahisley@gmail.com>
2021-12-27 16:27:58 -0500

    Initial commit

---
+++ lib/fns/readFunction.ts
@@ -0,0 +1,7 @@
+import type { APIGatewayProxyResultV2 } from 'aws-lambda';
+import { Notes, NoteType } from './notesTable';
+
+export const handler = async (): Promise<APIGatewayProxyResultV2> => {
+    const notes = await Notes.find({ pk: 'note' }, { limit: 10, reverse: true });
+    return { body: JSON.stringify(notes), statusCode: 200 };
+};
---
+++ lib/three-tier-app-stack.ts
@@ -0,0 +1,77 @@
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
+
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+
+export class CdkThreeTierServerlessStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    //Dynamo Table setup, including IAM access for read/write functions
+    const table = new Table(this, 'NotesTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      sortKey: { name: 'sk', type: AttributeType.STRING },
+      tableName: 'NotesTable',
+    });
+
+    const readFunction = new NodejsFunction(this, 'ReadNotesFn', {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/fns/readFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    const writeFunction = new NodejsFunction(this, 'WriteNoteFn', {
+      architecture: Architecture.ARM_64,
+      entry: `${__dirname}/fns/writeFunction.ts`,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    table.grantReadData(readFunction);
+    table.grantWriteData(writeFunction);
+
+    //HTTP Gateway setup for our API
+    const api = new HttpApi(this, 'NotesApi', {
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [CorsHttpMethod.GET, CorsHttpMethod.POST],
+        allowOrigins: ['*'],
+      },
+    });
+
+    const readIntegration = new HttpLambdaIntegration(
+      'ReadIntegration',
+      readFunction
+    );
+
+    const writeIntegration = new HttpLambdaIntegration(
+      'WriteIntegration',
+      writeFunction
+    );
+
+    api.addRoutes({
+      integration: readIntegration,
+      methods: [HttpMethod.GET],
+      path: '/notes',
+    });
+
+    api.addRoutes({
+      integration: writeIntegration,
+      methods: [HttpMethod.POST],
+      path: '/notes',
+    });
+
+    //Crn output so we can see and reuse our API URL (no custom domain in this setup)
+    new CfnOutput(this, 'HttpApiUrl', { value: api.apiEndpoint });
+  }
+}
